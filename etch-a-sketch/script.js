055996727a4babb8e03c800d70bccce55c50daba
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-01-14 17:15:38 +0000

    Updated with grid size changing

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,5 +1,6 @@
-function makeGrid () {
-    for (let i = 0; i < 30*30; i++) {
+function makeGrid(gridSize) {
+    grid.innerHTML = '';
+    for (let i = 0; i < gridSize*gridSize; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
         grid.appendChild(box);
@@ -20,10 +21,25 @@ function fillGrid(e) {
     }
 }
 
-const grid = document.getElementById('grid');
-const clearButton = document.getElementById('clear')
+function checkNum(input) {
+    if (isNaN(input) || input === '' || input === '0') {
+        alert("Invalid input. Must be a number");
+        return 30;
+    } else if (input > 100) {
+        alert("Invalid input. Must be a number <= 100");
+        return 30;
+    } else {
+        return parseInt(input);
+    }
+}
 
-makeGrid();
+
+const grid = document.getElementById('grid');
+const clearButton = document.getElementById('clear_grid');
+const changeButton = document.getElementById('change_grid');
+let gridSize = 30;
+
+makeGrid(gridSize);
 
 grid.addEventListener('mousedown', (e) => {
     e.preventDefault();
@@ -36,5 +52,13 @@ grid.addEventListener('mousedown', (e) => {
 
 clearButton.addEventListener('click', () => {
     clearGrid();
-    makeGrid();
+    makeGrid(gridSize);
 });
+
+changeButton.addEventListener('click', () => {
+    gridSize = checkNum(prompt("Please enter grid size (max 100)", 30));
+    grid.style.gridTemplateColumns = 'repeat(' + gridSize + ', 15px)';
+    grid.style.gridTemplateRows = 'repeat(' + gridSize + ', 15px)';
+    makeGrid(gridSize);
+});
+
