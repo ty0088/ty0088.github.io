b9a3df10ca83a469a428ab4d4b63b516a5d4b294
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-02-08 12:54:30 +0000

    Project complete
    No styling applied

--- library/script.js
+++ library/script.js
@@ -14,26 +14,59 @@ function refreshBookList() {
   document.getElementById("bookList").innerHTML = "";
   myLibrary.map(book => {  
     const newDiv = document.createElement("div");
-    newDiv.classList.add(myLibrary.indexOf(book));
-    const newContent = document.createTextNode("Title: " + book.title + ", Author: " + book.author + ", Pages: " + book.pages + ", Read?: " + book.read);
+    const remBtn = document.createElement("button");
+    const readBtn = document.createElement("button");
+    newDiv.setAttribute("id",myLibrary.indexOf(book));
+    remBtn.setAttribute("type","button");
+    remBtn.setAttribute("onclick","removeBook(this)");
+    readBtn.setAttribute("type","button");
+    readBtn.setAttribute("onclick","isRead(this)");
+    const newContent = document.createTextNode("Title: " + book.title + ", Author: " + book.author + ", No. of Pages: " + book.pages + ", Read?: " + book.read);
+    remBtn.innerHTML = "Remove Book";
+    readBtn.innerHTML = "Read/Not Read";
     newDiv.appendChild(newContent);
+    newDiv.appendChild(readBtn);
+    newDiv.appendChild(remBtn);
+
     document.getElementById("bookList").appendChild(newDiv);
   });
-  console.log("Refreshed Book List")
 }
 
 function newBook() {
-  
+  document.getElementById("form").style.display = "block";
+}
+
+function btnSub() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+
+  addBookToLibrary(title, author, pages, read);
+
+  document.getElementById("form").reset();
+  document.getElementById("form").style.display = "none";
+}
+
+function removeBook(e) {
+  let index = parseInt(e.parentNode.getAttribute("id"));
+  myLibrary.splice(index, 1);
+  refreshBookList();
+}
+
+function isRead(e) {
+  let index = parseInt(e.parentNode.getAttribute("id"));
+  if (myLibrary[index].read === true) {
+    myLibrary[index].read = false;
+  } else {
+    myLibrary[index].read = true;
+  }
+  refreshBookList();
 }
 
 let myLibrary = [];
 
-
-
-
-addBookToLibrary("Book1", "T.Yip", 10, true);
-addBookToLibrary("Book2", "T.Yip", 20, false);
-addBookToLibrary("Book3", "T.Yip", 30, false);
+addBookToLibrary("Example Book", "J.Blogs", 10, true);
 
 
 
