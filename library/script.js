2ac3039f871b926d213914b895f1d494d0f8cb8d
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-02-14 10:46:36 +0000

    refactor to use class

--- library/script.js
+++ library/script.js
@@ -1,8 +1,10 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
