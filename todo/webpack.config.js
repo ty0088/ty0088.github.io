dfb753e479cf7968f50653fcf7949690fee27b3d
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-03-03 15:28:58 +0000

    Todo Project
    Website html design/template

---
+++ todo/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      //title: 'Output Management',
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
