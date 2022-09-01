a8c9ebfc8122518901c12dfb46519aa3b3df9355
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-09-01 15:52:05 +0100

    Update Shopping Cart
    Added testing for Layout Component

--- shopping_cart/src/Tests/Layout.test.js
+++ shopping_cart/src/Tests/Layout.test.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import Layout from '../Components/Layout';
 
-jest.mock()
+
+const currPage = 'Home';
+const currPageClick = jest.fn();
+
+jest.mock('../Components/Nav', () => ({currPageClick}) => (
+    <>
+        <button onClick={currPageClick}>Link</button>
+    </>
+));
 
 describe("Layout component", () => {
-    // it("Home page renders", () => {
-    //   render(<App />);
-    //   expect(screen.getByRole("heading").textContent).toMatch(/Home/i);
-    // });
+    it("Heading renders Home", () => {
+        render(<Layout currPage={currPage} currPageClick={currPageClick}/>);
+        expect(screen.getAllByRole("heading")[1].textContent).toMatch(/home/i);
+    });
+
+    it("Nav link click", () => {
+        render(<Layout currPage={currPage} currPageClick={currPageClick}/>);
+        userEvent.click(screen.getByRole("button"));
+        expect(currPageClick).toHaveBeenCalledTqimes(1);
+    });
 });
