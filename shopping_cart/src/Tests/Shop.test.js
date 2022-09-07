3e096adeb42112652aa4f7d2eeb0691d6a6739fd
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-09-07 09:59:51 +0100

    Update to Shopping Cart
    Shop component shop test updated to allow for array input
    Still to add:
    -Click picture to bring up item details in new page or pop up
    -Click Cart Icon to bring up Cart pop up
    -Sort shop items by price button

--- shopping_cart/src/Tests/Shop.test.js
+++ shopping_cart/src/Tests/Shop.test.js
@@ -9,24 +9,26 @@ const addToCart = jest.fn();
 
 describe("Shop component", () => {
     it("Shop renders no items", () => {
-        const shopItems = ({});
+        const shopItems = ([]);
         render(<Shop shopItems={shopItems} />, {wrapper: MemoryRouter});
         expect(screen.getByText(/sorry/i)).toBeVisible();
     });
 
     it("Shop has item and add to cart", () => {
-        const shopItems = ({
-            "1": {
-            "name": "Mittens",
-            "type": "American Wirehair",
-            "sex": "Male",
-            "currency": "£",
-            "price": 600,
-            "age": "2 Months",
-            "likes": "Purring, eating, playing",
-            "qty available" : 1,
-            "available": true
-        }});
+        const shopItems = ([
+            {   
+                "item num": "1",
+                "name": "Mittens",
+                "type": "American Wirehair",
+                "sex": "Male",
+                "currency": "£",
+                "price": 600,
+                "age": "2 Months",
+                "likes": "Purring, eating, playing",
+                "qty available" : 1,
+                "available": true
+            }
+        ]);
         render(<Shop shopItems={shopItems} clickAddBtn={addToCart}/>, {wrapper: MemoryRouter});
         userEvent.click(screen.getByRole("button", {name: "Add to Cart"}));
         expect(addToCart).toHaveBeenCalledTimes(1);
@@ -35,18 +37,20 @@ describe("Shop component", () => {
     });
 
     it("Cat is unavailable", () => {
-        const shopItems = ({
-            "1": {
-            "name": "Mittens",
-            "type": "American Wirehair",
-            "sex": "Male",
-            "currency": "£",
-            "price": 600,
-            "age": "2 Months",
-            "likes": "Purring, eating, playing",
-            "qty available" : 1,
-            "available": false
-        }});
+        const shopItems = ([
+            {   
+                "item num": "1",
+                "name": "Mittens",
+                "type": "American Wirehair",
+                "sex": "Male",
+                "currency": "£",
+                "price": 600,
+                "age": "2 Months",
+                "likes": "Purring, eating, playing",
+                "qty available" : 1,
+                "available": false
+            }
+        ]);
         render(<Shop shopItems={shopItems} clickAddBtn={addToCart}/>, {wrapper: MemoryRouter});
         expect(screen.getByText(/currently unavailable/i)).toBeVisible();
         expect(screen.queryByRole("button", {name: "Add to Cart"})).not.toBeInTheDocument();
