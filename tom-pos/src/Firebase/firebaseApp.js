d366029df1c2cd3f3058a823da14b996a253b181
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-09-22 18:53:36 +0100

    Added Firebase components

---
+++ tom-pos/src/Firebase/firebaseApp.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1WcKoTTTb78ZEsA-A3P07K0j9gy4r_3M",
+  authDomain: "tom-pos.firebaseapp.com",
+  projectId: "tom-pos",
+  storageBucket: "tom-pos.appspot.com",
+  messagingSenderId: "48668345969",
+  appId: "1:48668345969:web:630f238ecf371b2127bdd0"
+};
+
+// Initialize Firebase
+const fireApp = initializeApp(firebaseConfig);
+
+export default fireApp;
---
+++ tom-pos/src/Firebase/firebaseAuth.js
@@ -0,0 +1,60 @@
+import { 
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "firebase/auth";
+
+const auth = getAuth();
+
+const signUpEmail = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+};
+
+const signIn = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+};
+
+const signUp = () => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+};
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
+
+export { signUpEmail, signIn, signUp };
