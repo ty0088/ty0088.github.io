e6fe4761a5fa0eb636a10658261bf3f59db1ec1a
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-09-22 11:53:34 +0100

    Added tom-pos project

---
+++ tom-pos/src/App.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, Outlet, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
