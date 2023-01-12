ba5ecb8f13a8af6f926f74609a9823d28aa90006
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-01-12 20:23:08 +0000

    Added missing reference to setRootData

--- tom-pos/src/Components/AuthenticatePopUp.js
+++ tom-pos/src/Components/AuthenticatePopUp.js
@@ -20,7 +20,7 @@ const AuthenticatePopUp = ({cancelClick, changeType, confirmEmailChange, confirm
             if (result) {
                 if (changeType === 'email') {
                     await confirmEmailChange(input1);
-                    setRootData({...userData, 'account-email': input1});
+                    setRootData({...userData, 'account-email': input1}, 'user-data');
                     alert('Email successfully changed, you will now be signed out, please sign back in');
                     signOutAcc();
                 } else {
