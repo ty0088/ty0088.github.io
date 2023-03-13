d97f4813b8ff0d49938d08d2a5df56b6ae0012a6
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-13 17:29:10 +0000

    added authentication routes

--- members_messageboard/routes/index.js
+++ members_messageboard/routes/index.js
@@ -1,12 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Require controller modules.
+const authenticationController = require('../controllers/authenticationController');
 
-/* GET home page - redirect. */
+//GET: home page - redirect
 router.get('/', function(req, res, next) {
   res.redirect('/messages');
 });
+
+//log in/out routes
+//GET: log in form
+router.get('/log-in', authenticationController.log_in_get);
+
+//POST: log in form
+router.post('/log-in', authenticationController.log_in_post);
+
+//POST: log out form
+router.get('/log-out', authenticationController.log_out);
+
 
 //message routes
 //GET: create message form
@@ -29,6 +41,7 @@ router.post('/message/:id/delete', );
 
 //GET: home page which displays all messages posted
 router.get('/messages', );
+
 
 //user routes
 //GET: create user form
