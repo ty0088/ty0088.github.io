40758f8546c8cd7d16c660e7b7140814878763cc
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-13 17:29:25 +0000

    Create user.js

---
+++ members_messageboard/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, maxLength: 10 },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    joinDate: { type: Date, default: Date.now },
+    membershipStatus: { 
+        type: String,
+        required: true,
+        enum: ["Member", "Banned", "Deleted"],
+        default: "Member"
+    }
+});
+
+// Virtual URL
+UserSchema.virtual("url").get(function () {
+    return `/user/${this._id}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
