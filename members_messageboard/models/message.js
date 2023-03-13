a5309f28ad71b956baf4c3af3a50addd27f07d60
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-13 17:29:29 +0000

    Create message.js

---
+++ members_messageboard/models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    joinDate: { type: Date, default: Date.now },
+    text: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
