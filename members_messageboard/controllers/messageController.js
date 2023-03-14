32919935b2aecd9e959d870470106d85f5667d8a
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-14 09:00:08 +0000

    added message list cb

--- members_messageboard/controllers/messageController.js
+++ members_messageboard/controllers/messageController.js
@@ -2,4 +2,19 @@ const async = require('async');
 const { body, validationResult } = require('express-validator');
 
 //import models
+const User = require('../models/user');
+const Message = require('../models/message');
 
+exports.message_list = async (req, res, next) => {
+    try {
+        const messages = await Message.find({});
+        //render message list page
+        res.render('message_list', {
+            title: 'Message Board',
+            messages: messages
+        });
+    } catch (error) {
+        //pass on any errors
+        return next(error);
+    }
+};
