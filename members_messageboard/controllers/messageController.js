3c1ce352373a9eed242260226ee07c642f2dda80
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-14 17:25:54 +0000

    added message create get and post modules

--- members_messageboard/controllers/messageController.js
+++ members_messageboard/controllers/messageController.js
@@ -2,12 +2,13 @@ const async = require('async');
 const { body, validationResult } = require('express-validator');
 
 //import models
-const User = require('../models/user');
+// const User = require('../models/user'); -----------delete
 const Message = require('../models/message');
 
+//render all messages page on GET
 exports.message_list = async (req, res, next) => {
     try {
-        const messages = await Message.find({});
+        const messages = await Message.find({}).populate('user');
         //render message list page
         res.render('message_list', {
             title: 'Message Board',
@@ -18,3 +19,49 @@ exports.message_list = async (req, res, next) => {
         return next(error);
     }
 };
+
+//render create message page on GET
+exports.message_create_get = (req, res, next) => {
+    res.render('message_form', {
+        title: 'Message Board - New Post',
+        goToUrl: 'goToUrl("/")'
+    });
+};
+
+//handle create message on POST
+exports.message_create_post = [
+    //sanitise and validate input
+    body('messageText', 'A message is required to post and cannot exceed 1000 characters.')
+        .trim()
+        .notEmpty()
+        .isLength({ max: 1000 })
+        .escape(),
+    //process request after validation and sanitisation.
+    async (req, res, next) => {
+        try {
+            //extract the validation errors from a request.
+            const errors = validationResult(req);
+            //create a new message object
+            const message = new Message({
+                user: req.user,
+                text: req.body.messageText
+            });
+            //check if there are errors present
+            if (!errors.isEmpty()) {
+                //if there are errors render message form again with errors and any previously entered data
+                res.render('message_form', {
+                    title: 'Message Board - New Post',
+                    goToUrl: 'goToUrl("/")',
+                    currMessage: message,
+                    errors: errors.array()
+                });
+            } else {
+                // if no errors then save message to db and redirect to home
+                await message.save();
+                res.redirect('/');
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+];
