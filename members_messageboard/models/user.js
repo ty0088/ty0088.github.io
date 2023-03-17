98ad72012256294f5e533ef3697d59a288717448
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-17 13:43:29 +0000

    increased username character limit to 20

--- members_messageboard/models/user.js
+++ members_messageboard/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true, maxLength: 10 },
+    username: { type: String, required: true, maxLength: 20 },
     password: { type: String, required: true, minLength: 1 },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
