b86e7defdca2bc89e53bc950677ebb98df4d503f
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-17 16:21:56 +0000

    Added message update CBs and set admin/mod privileges

--- members_messageboard/controllers/messageController.js
+++ members_messageboard/controllers/messageController.js
@@ -7,7 +7,7 @@ const Message = require('../models/message');
 //render all messages page on GET
 exports.message_list = async (req, res, next) => {
     try {
-        const messages = await Message.find({}).populate('user').sort({ postDate: -1 });
+        const messages = await Message.find({}).populate('user lastEditBy').sort({ postDate: -1 });
         //render message list page
         res.render('message_list', {
             title: 'Message Board',
@@ -21,10 +21,18 @@ exports.message_list = async (req, res, next) => {
 
 //render create message page on GET
 exports.message_create_get = (req, res, next) => {
-    res.render('message_form', {
-        title: 'Message Board - New Post',
-        goToUrl: 'goToUrl("/")'
-    });
+    //check user is logged in
+    if (req.user) {
+        res.render('message_form', {
+            title: 'Message Board - New Post',
+            goToUrl: 'goToUrl("/")'
+        });
+    } else {
+        //no user, throw error
+        let err = new Error("Unauthorised request - Insufficient privileges");
+        err.status = 401;
+        return next(err);
+    }
 };
 
 //handle create message on POST
@@ -38,6 +46,12 @@ exports.message_create_post = [
     //process request after validation and sanitisation.
     async (req, res, next) => {
         try {
+            //check user is logged in, otherwise throw error
+            if (!req.user) {
+                let err = new Error("Unauthorised request - Insufficient privileges");
+                err.status = 401;
+                return next(err);
+            }
             //extract the validation errors from a request.
             const errors = validationResult(req);
             //create a new message object
@@ -68,8 +82,85 @@ exports.message_create_post = [
 //render update form on GET
 exports.message_update_get = async (req, res, next) => {
     try {
-        
+        //query db for specific message
+        const message = await Message.findById(req.params.id).populate('user');
+        //check if message was found
+        if (message == null) {
+            //if message not found, return error
+            const err = new Error("Post not found");
+            err.status = 404;
+            return next(err);
+        }
+        //verify user has sufficient privileges to do this action
+        console.log(req.user.membershipStatus);
+        console.log(message.user.membershipStatus);
+        if (req.user.membershipStatus == 'Admin' || (req.user.membershipStatus == 'Mod' && message.user.membershipStatus != 'Admin')) {
+            //admin/mod privileges (but mod cannot edit admin)
+            res.render('message_form', {
+                title: 'Message Board - New Post',
+                currMessage: message,
+                goToUrl: 'goToUrl("/")'
+            });
+        } else if (req.user && (message.user._id.toString() == req.user._id)) {
+            //user is logged in and message belongs to req user
+            res.render('message_form', {
+                title: 'Message Board - New Post',
+                currMessage: message,
+                goToUrl: 'goToUrl("/")'
+            });
+        } else {
+            //no user or message user id and user id does not match, throw error
+            let err = new Error("Unauthorised request - Insufficient privileges");
+            err.status = 401;
+            return next(err);
+        }
     } catch (error) {
         next(error);
     }
 };
+
+exports.message_update_post = [
+    //sanitise and validate input
+    body('messageText', 'A message is required to post and cannot exceed 1000 characters.')
+        .trim()
+        .notEmpty()
+        .isLength({ max: 1000 })
+        .escape(),
+
+    //process request after validation and sanitisation.
+    async (req, res, next) => {
+        //verify user has sufficient privileges to do this action
+        //query db for specific message
+        const message = await Message.findById(req.params.id);
+        //check if message was found
+        if (message == null) {
+            //if message not found, return error
+            const err = new Error("Post not found");
+            err.status = 404;
+            return next(err);
+        }
+        //verify user has sufficient privileges to do this action
+        if (req.user.membershipStatus == 'Admin' || (req.user.membershipStatus == 'Mod' && message.user.membershipStatus != 'Admin')) {
+            //admin/mod privileges (but mod cannot edit admin)
+            if (message.user.membershipStatus == 'Admin')
+            message.lastEditDate = new Date();
+            message.text = req.body.messageText;
+            message.lastEditBy = req.user;
+            await Message.findByIdAndUpdate(req.params.id, message, {});
+            res.redirect('/');
+        } else if (req.user && (message.user._id.toString() == req.user._id)) {
+            //user is logged in and message belongs to req user
+            message.lastEditDate = new Date();
+            message.text = req.body.messageText;
+            message.lastEditBy = req.user;
+            await Message.findByIdAndUpdate(req.params.id, message, {});
+            res.redirect('/');
+        } else {
+            //no user or message user id and user id does not match, throw error
+            let err = new Error("Unauthorised request - Insufficient privileges");
+            err.status = 401;
+            return next(err);
+        }
+
+    }
+];
