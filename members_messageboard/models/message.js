0516f18b4450ca6bab780f30b7f7618887c726e5
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-17 16:23:04 +0000

    added last edit info

--- members_messageboard/models/message.js
+++ members_messageboard/models/message.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     postDate: { type: Date, default: Date.now },
+    lastEditDate: { type: Date, default: null },
+    lastEditBy: { type: Schema.Types.ObjectId, ref: "User" },
     text: { type: String, required: true }
 });
 
