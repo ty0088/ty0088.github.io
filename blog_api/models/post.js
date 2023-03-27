604f0f165c082001286de93a02845e0fc7030c09
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-27 15:47:07 +0100

    added mongoose-paginate-v2 and last edit keys

--- blog_api/models/post.js
+++ blog_api/models/post.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
     text: { type: String, required: true },
     tite: { type: String, maxLength: 30 },
-    private: { type: Boolean, default: false },
-    post_date: { type: Date, default: Date.now },
+    private: { type: Boolean, default: false, required: true },
+    post_date: { type: Date, default: Date.now, required: true },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    lastEditDate: { type: Date },
+    lastEditBy: { 
+        type: Schema.Types.ObjectId, 
+        ref: "User", 
+        required: function() {
+            console.log(this.lastEditDate);
+            return this.lastEditDate;
+        }
+    },
 });
+
+PostSchema.plugin(mongoosePaginate);
 
 // Virtual URL
 PostSchema.virtual("url").get(function () {
