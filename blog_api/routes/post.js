bd17456c9b45d39020b9679513155baf92fe0a3c
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-27 14:57:20 +0100

    Post routes added

---
+++ blog_api/routes/post.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const postController = require('../controllers/postController');
+
+//post routes
+//POST create new blog post - protected
+router.post('/create', postController.create_post_post);
+
+//PUT update blog post - protected
+router.post('/:id/update', );
+
+//DELETE delete blog post - protected
+router.delete('/:id/delete', );
+
+//GET retrieve blog post and comments - protected
+router.get('/:id', );
+
+//GET list of blog posts (limit to 10)
+router.get('/', );
+
+module.exports = router;
