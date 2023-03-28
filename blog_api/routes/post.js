3ccddf251a6254b7e0ad86250b853f456bb0a344
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-28 12:41:50 +0100

    added post update route

--- blog_api/routes/post.js
+++ blog_api/routes/post.js
@@ -5,10 +5,10 @@ const postController = require('../controllers/postController');
 
 //post routes
 //POST create new blog post - protected
-router.post('/create', postController.create_post_post);
+router.post('/create', postController.post_create_post);
 
 //PUT update blog post - protected
-router.post('/:id/update', );
+router.put('/:id/update', postController.post_update_put);
 
 //DELETE delete blog post - protected
 router.delete('/:id/delete', );
@@ -16,7 +16,7 @@ router.delete('/:id/delete', );
 //GET retrieve blog post and comments - protected
 router.get('/:id', );
 
-//GET list of blog posts (limit to 10)
+//GET list of blog posts
 router.get('/', postController.post_list_get);
 
 module.exports = router;
