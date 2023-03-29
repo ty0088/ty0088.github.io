f41a8903d224bb99a2df6cea9a58f77658d74bb3
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-29 12:11:22 +0100

    created comment routes

---
+++ blog_api/routes/comment.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const commentController = require('../controllers/commentController');
+
+//comment routes
+//POST create new comment for blog post - protected
+router.post('/:postId/create', commentController);
+
+//PUT update comment - protected
+router.put('/:commentId/update', commentController);
+
+//DELETE delete blog post - protected
+router.delete('/:commentId/delete', commentController);
+
+//GET retrieve blog post and comments - protected
+router.get('/:commentId', commentController);
+
+//GET list of blog posts
+router.get('/', commentController);
+
+module.exports = router;
