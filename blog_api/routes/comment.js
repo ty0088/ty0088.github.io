dee41e06e4e8897e02e67795b0e1a0cb79c7c43d
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-29 15:31:28 +0100

    added comment_create_post route

--- blog_api/routes/comment.js
+++ blog_api/routes/comment.js
@@ -5,18 +5,18 @@ const commentController = require('../controllers/commentController');
 
 //comment routes
 //POST create new comment for blog post - protected
-router.post('/:postId/create', commentController.comment_create_post);
+router.post('/:postId/comment/create', commentController.comment_create_post);
 
-//PUT update comment - protected
-router.put('/:commentId/update', commentController);
+// //PUT update comment - protected
+// router.put('/:commentId/update', commentController);
 
-//DELETE delete blog post - protected
-router.delete('/:commentId/delete', commentController);
+// //DELETE delete blog post - protected
+// router.delete('/:commentId/delete', commentController);
 
-//GET retrieve blog post and comments - protected
-router.get('/:commentId', commentController);
+// //GET retrieve blog post and comments - protected
+// router.get('/:commentId', commentController);
 
-//GET list of blog posts
-router.get('/', commentController);
+// //GET list of blog posts
+// router.get('/', commentController);
 
 module.exports = router;
