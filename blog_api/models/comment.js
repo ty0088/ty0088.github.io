407c4db993d91ab561516e6222f4422d030cbd39
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-29 16:42:31 +0100

    added last edit by and date keys

--- blog_api/models/comment.js
+++ blog_api/models/comment.js
@@ -7,6 +7,14 @@ const CommentSchema = new Schema({
     post_date: { type: Date, default: Date.now },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     post: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
+    lastEditDate: { type: Date },
+    lastEditBy: { 
+        type: Schema.Types.ObjectId, 
+        ref: "User", 
+        required: function() {
+            return this.lastEditDate;
+        }
+    },
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
