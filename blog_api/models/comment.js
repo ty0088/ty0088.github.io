535b0f8821792dddf565784fdc93c60976e8acf3
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-29 11:08:10 +0100

    changed key "blog" to "post"

--- blog_api/models/comment.js
+++ blog_api/models/comment.js
@@ -6,7 +6,7 @@ const CommentSchema = new Schema({
     text: { type: String, required: true },
     post_date: { type: Date, default: Date.now },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
 });
 
 // Virtual URL
