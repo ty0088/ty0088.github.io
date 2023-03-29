8bbd0ef7054afc4a38549563942c5a7f8ceb63d7
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-29 13:53:23 +0100

    added comment_create_post route

--- blog_api/routes/comment.js
+++ blog_api/routes/comment.js
@@ -5,7 +5,7 @@ const commentController = require('../controllers/commentController');
 
 //comment routes
 //POST create new comment for blog post - protected
-router.post('/:postId/create', commentController);
+router.post('/:postId/create', commentController.comment_create_post);
 
 //PUT update comment - protected
 router.put('/:commentId/update', commentController);
