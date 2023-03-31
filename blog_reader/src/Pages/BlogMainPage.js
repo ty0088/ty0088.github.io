11b5f2d2f29a7dd61fd52d37c3632d6de58f890c
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-03-31 18:07:00 +0100

    fetch blog post list from api and render

--- blog_reader/src/Pages/BlogMainPage.js
+++ blog_reader/src/Pages/BlogMainPage.js
@@ -1,10 +1,51 @@
 import React, { useState, useEffect } from 'react';
 
 const BlogMainPage = () => {
+    const [postList, setPostList] = useState([]);
+    const [paginateInfo, setPaginateInfo] = useState({});
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const fetchResponse = async () => {
+            try {
+                //on initial render, fetch blog post list from blog api
+                const response = await fetch(`${process.env.REACT_APP_BLOGAPI_ORIGIN}/post`);
+                const responseData = await response.json();
+                //once response is returned, set the post list and paginate data to state
+                setPostList(responseData.docs);
+                setPaginateInfo({
+                    totalDocs: responseData.totalDocs,
+                    limit: responseData.limit,
+                    totalPages: responseData.totalPages,
+                    page: responseData.page,
+                    pagingCounter: responseData.page,
+                    hasPrevPage: responseData.hasPrevPage,
+                    hasNextPage: responseData.hasNextPage,
+                    prevPage: responseData.prevPage,
+                    nextPage: responseData.nextPage,
+                });
+            } catch (error) {
+                console.log(error);
+                //set error state to render to page----------- ??
+                setError(error);
+            }
+        };
+        fetchResponse();
+    }, []);
 
     return (
         <div>
-            This is the main page
+            {postList.length > 0 &&
+                postList.map((post, i) => {
+                    return (
+                        <div key={i}>
+                            <h3>{post.title}</h3>
+                            <p>{post.text}</p>
+                        </div>
+                    );
+                })
+            }
+            
         </div>
     );
 };
