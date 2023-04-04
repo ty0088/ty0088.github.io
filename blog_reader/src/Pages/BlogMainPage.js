82caf1e1fae83c01cd4a5038bfffe75e07d89974
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-04 15:28:15 +0100

    get user state on render for conditional rendering

--- blog_reader/src/Pages/BlogMainPage.js
+++ blog_reader/src/Pages/BlogMainPage.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import fetchUser from '../javascript/fetchUser';
+import logOut from '../javascript/logOut';
 
 const BlogMainPage = () => {
     const [postList, setPostList] = useState([]);
     const [paginateInfo, setPaginateInfo] = useState({});
-    const [error, setError] = useState();
+    const [user, setUser] = useState(null);
 
+    //on initial render, check if user is logged in, then get blog post list from api
     useEffect(() => {
-        const fetchResponse = async () => {
+        //get user credentials if any and get blog list
+        const fetchAuthBlogList = async () => {
             try {
-                //on initial render, fetch blog post list from blog api
-                const response = await fetch(`${process.env.REACT_APP_BLOGAPI_ORIGIN}/post`);
+                //request user
+                const userData = await fetchUser();
+                setUser(userData.user);
+                //request blog list and include our credentials (cookie)
+                const response = await fetch(`${process.env.REACT_APP_BLOGAPI_URL}/post`, { credentials: "include" });
                 const responseData = await response.json();
                 //once response is returned, set the post list and paginate data to state
                 setPostList(responseData.docs);
@@ -27,25 +36,37 @@ const BlogMainPage = () => {
             } catch (error) {
                 console.log(error);
                 //set error state to render to page----------- ??
-                setError(error);
             }
         };
-        fetchResponse();
+        fetchAuthBlogList();
     }, []);
 
     return (
         <div>
-            {postList.length > 0 &&
-                postList.map((post, i) => {
-                    return (
-                        <div key={i}>
-                            <h3>{post.title}</h3>
-                            <p>{post.text}</p>
-                        </div>
-                    );
-                })
-            }
-            
+            <h1>The Blog Spot</h1>
+            <nav>
+                {!user &&
+                    <Link to='/blog_reader/log-in'>Log In</Link>
+                }
+                {user &&
+                    <button type='button' onClick={logOut}>Log Out</button>
+                }
+            </nav>
+            <div>
+                {postList.length > 0 &&
+                    postList.map((post, i) => {
+                        return (
+                            <div key={i}>
+                                <h3>{post.title}</h3>
+                                <p>{post.text}</p>
+                                {typeof post.lastEditBy == 'object' &&
+                                    <p>Last edited: {new Date(post.lastEditDate).toLocaleString()} by {post.lastEditBy.display_name} ({post.lastEditBy.user_type})</p>
+                                }
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 };
