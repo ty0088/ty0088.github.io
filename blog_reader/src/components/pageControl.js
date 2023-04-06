52ce74fbc6cab9f617f0504c9ab8558d21d91b24
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-06 19:09:34 +0100

    Create pageControl.js

---
+++ blog_reader/src/components/pageControl.js
@@ -0,0 +1,18 @@
+import '../styles/BlogMainPage.css'
+import React from 'react';
+
+import PageNavSpan from './PageNavSpan';
+
+const PageControl = ({ paginateInfo, pageNum, sortOrd, limitVal }) => {
+
+    return (
+        <div className='post-info'>
+            <PageNavSpan paginateInfo={paginateInfo} sortOrd={sortOrd} limitVal={limitVal} />
+            &nbsp;-&nbsp;
+            Sort: <a href={`/blog_reader?page=${pageNum}&sortOrd=-1&limit=${limitVal}`}>{(sortOrd === '-1' || sortOrd === '') ? <strong>Newest First</strong> : 'Newest First'}</a> : <a href={`/blog_reader?page=${pageNum}&sortOrd=1&limit=${limitVal}`}>{sortOrd === '1' ? <strong>Oldest First</strong> : 'Oldest First'}</a>
+            &nbsp;-&nbsp;
+            Posts: <a href={`/blog_reader?page=1&sortOrd=${sortOrd}&limit=5`}>{(limitVal === '5' || limitVal === '') ? <strong>5</strong> : '5'}</a> / <a href={`/blog_reader?page=1&sortOrd=${sortOrd}&limit=10`}>{limitVal === '10' ? <strong>10</strong> : '10'}</a> / <a href={`/blog_reader?page=1&sortOrd=${sortOrd}&limit=20`}>{limitVal === '20' ? <strong>20</strong> : '20'}</a>
+        </div>
+    );
+};
+export default PageControl;
