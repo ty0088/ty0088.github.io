755ddb34f791e94d8625c888dac8e9ac11fe5180
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-11 16:25:00 +0100

    Added path for updating user details

--- blog_reader/src/App.js
+++ blog_reader/src/App.js
@@ -7,19 +7,20 @@ import BlogMainPage from './Pages/BlogMainPage';
 import LogInPage from './Pages/LogInPage';
 import PostDetailPage from './Pages/PostDetailPage';
 import PostFormPage from './Pages/PostFormPage';
-import SignUpPage from './Pages/SignUpPage';
+import UserFormPage from './Pages/UserFormPage';
 import UserDetailPage from './Pages/UserDetailPage';
 
 function App() {
   return (
     <Routes>
-      <Route path='/blog_reader' element={<BlogMainPage />}/>
-      <Route path='/blog_reader/create-post' element={<PostFormPage action={'create'}/>}/>
-      <Route path='/blog_reader/update-post' element={<PostFormPage action={'update'}/>}/>
-      <Route path='/blog_reader/post/:postId' element={<PostDetailPage />}/>
-      <Route path='/blog_reader/sign-up' element={<SignUpPage />}/>
+      <Route path='/blog_reader' element={<BlogMainPage />} />
+      <Route path='/blog_reader/create-post' element={<PostFormPage action={'create'} />} />
+      <Route path='/blog_reader/update-post' element={<PostFormPage action={'update'} />} />
+      <Route path='/blog_reader/post/:postId' element={<PostDetailPage />} />
       <Route path='/blog_reader/log-in' element={<LogInPage />}/>
-      <Route path='/blog_reader/user/:userId' element={<UserDetailPage />}/>
+      <Route path='/blog_reader/sign-up' element={<UserFormPage action={'create'} />} />
+      <Route path='/blog_reader/user/:userId/update' element={<UserFormPage action={'update'} />} />
+      <Route path='/blog_reader/user/:userId' element={<UserDetailPage />} />
     </Routes>
   );
 }
