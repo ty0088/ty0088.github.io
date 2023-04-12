1817267c380a515df3fdf52f2e7431d5a1f223b0
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-12 14:56:52 +0100

    formatting change

--- blog_reader/src/components/PageNavSpan.js
+++ blog_reader/src/components/PageNavSpan.js
@@ -11,11 +11,11 @@ const PageNavSpan = ({ paginateInfo, sortOrd, limitVal, classStr }) => {
     //format example 1 > 2 --- 2 total pages, current page is 1
     return (
         <span className={classStr}>
-            Page: {(paginateInfo.prevPage !== 1 && paginateInfo.page !== 1) && <a href={`/blog_reader?page=1&sortOrd=${sortOrd}&limit=${limitVal}`}>1</a>}{(paginateInfo.prevPage !== 1 && paginateInfo.page !== 1) && `/ `}
+            Page: {(paginateInfo.prevPage !== 1 && paginateInfo.page !== 1) && <a href={`/blog_reader?page=1&sortOrd=${sortOrd}&limit=${limitVal}`}>1</a>}{(paginateInfo.prevPage !== 1 && paginateInfo.page !== 1) && ` / `}
             {paginateInfo.hasPrevPage && <a href={`/blog_reader?page=${paginateInfo.prevPage}&sortOrd=${sortOrd}&limit=${limitVal}`}>{paginateInfo.prevPage}</a>}  
             {paginateInfo.hasPrevPage && ` < `} <strong>{paginateInfo.page}</strong> {paginateInfo.hasNextPage && ` > `}
             {paginateInfo.hasNextPage && <a href={`/blog_reader?page=${paginateInfo.nextPage}&sortOrd=${sortOrd}&limit=${limitVal}`}>{paginateInfo.nextPage}</a>}
-            {(paginateInfo.nextPage !== paginateInfo.totalPages && paginateInfo.page !== paginateInfo.totalPages) && `/ `} {(paginateInfo.nextPage !== paginateInfo.totalPages && paginateInfo.page !== paginateInfo.totalPages) && <a href={`/blog_reader?page=${paginateInfo.totalPages}&sortOrd=${sortOrd}&limit=${limitVal}`}>{paginateInfo.totalPages}</a>}
+            {(paginateInfo.nextPage !== paginateInfo.totalPages && paginateInfo.page !== paginateInfo.totalPages) && ` / `} {(paginateInfo.nextPage !== paginateInfo.totalPages && paginateInfo.page !== paginateInfo.totalPages) && <a href={`/blog_reader?page=${paginateInfo.totalPages}&sortOrd=${sortOrd}&limit=${limitVal}`}>{paginateInfo.totalPages}</a>}
         </span>
     );
 };
