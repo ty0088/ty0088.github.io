9af3a6af981203278f25e3798d27eea561ff79c3
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-12 10:42:41 +0100

    added header and sign up/cancel links...
    moved userLogIn fn inside of useEffect

--- blog_reader/src/Pages/LogInPage.js
+++ blog_reader/src/Pages/LogInPage.js
@@ -1,44 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useNavigate, Link } from "react-router-dom";
 
 const LogInPage = () => {
     const navigate = useNavigate();
 
+    //attach submit event listener
     useEffect(() => {
+        //request log in to api function
+        const userLogIn = async (e) => {
+            e.preventDefault();
+            try {
+                const email = document.getElementById('input-email').value;
+                const password = document.getElementById('input-password').value;
+                //fetch post request to log in and get token
+                const response = await fetch(`${process.env.REACT_APP_BLOGAPI_URL}/user/log-in`, {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email, password }),
+                });
+                //if successful response, redirect to home page
+                if (response.status === 200) {
+                    navigate("/blog_reader");
+                }
+            } catch (error) {
+                console.log(error);
+                //render error -------------------------------------
+            }
+        };
+
+        //call log in function when form is submitted
         document.addEventListener('submit', userLogIn);
 
+        //clean up
         return () => {
             document.removeEventListener('submit', userLogIn);
         };
+
+    // eslint-disable-next-line
     }, []);
 
-    const userLogIn = async (e) => {
-        e.preventDefault();
-        try {
-            const email = document.getElementById('input-email').value;
-            const password = document.getElementById('input-password').value;
-            //fetch post request to log in and get token
-            const response = await fetch(`${process.env.REACT_APP_BLOGAPI_URL}/user/log-in`, {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
-            //if successful response, redirect to home page
-            if (response.status === 200) {
-                navigate("/blog_reader");
-            }
-        } catch (error) {
-            console.log(error);
-            //render error -------------------------------------
-        }
-    };
-
     return (
-        <div>
+        <div id='main-container'>
+            <h1>The Blog Spot</h1>
+            <h3>Log In</h3>
             <form action='' method=''>
                 <div>
                     <label htmlFor='email'>Email: </label>
@@ -48,7 +56,9 @@ const LogInPage = () => {
                     <label htmlFor='password'>Password: </label>
                     <input type='password' id='input-password' name='password'/>
                 </div>
-                <button type='submit'>Log In</button>
+                <button className='button-link' type='submit'>Log In</button>
+                <Link to='/blog_reader'>Cancel</Link>
+                <Link to='/blog_reader/sign-up'>Sign Up</Link>
             </form>
         </div>
     );
