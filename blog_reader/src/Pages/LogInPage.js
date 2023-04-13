c4aa22c4939e3d970f82e750e5e27b536a5c91a2
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-13 12:40:48 +0100

    log in error rendered for user

--- blog_reader/src/Pages/LogInPage.js
+++ blog_reader/src/Pages/LogInPage.js
@@ -1,42 +1,55 @@
-import '../styles/formPages.css'
-import React, { useEffect } from 'react';
+import '../styles/formPages.css';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 
 const LogInPage = () => {
+    const [errorData, setErrorData] = useState(null);
     const navigate = useNavigate();
 
-    //attach submit event listener
-    useEffect(() => {
-        //request log in to api function
-        const userLogIn = async (e) => {
-            e.preventDefault();
-            try {
-                const email = document.getElementById('input-email').value;
-                const password = document.getElementById('input-password').value;
-                //fetch post request to log in and get token
-                const response = await fetch(`${process.env.REACT_APP_BLOGAPI_URL}/user/log-in`, {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ email, password }),
-                });
+    //function to request log in to api
+    const userLogIn = async (e) => {
+        e.preventDefault();
+        try {
+            const email = document.getElementById('input-email').value;
+            const password = document.getElementById('input-password').value;
+            //fetch post request to log in and get token
+            const response = await fetch(`${process.env.REACT_APP_BLOGAPI_URL}/user/log-in`, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            if (response.status === 200) {
                 //if successful response, redirect to home page
-                if (response.status === 200) {
-                    navigate("/blog_reader");
-                }
-            } catch (error) {
-                console.log(error);
-                //render error -------------------------------------
+                navigate("/blog_reader");
+            } else {
+                //if not successful response, set error data for rendering
+                const responseData = await response.json();
+                setErrorData(responseData.error);
             }
-        };
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-        //call log in function when form is submitted
+    //function to clear error data
+    const clearError = () => {
+        setErrorData(null);
+    };
+    
+    useEffect(() => {
+        //attach log in submit event listener
         document.addEventListener('submit', userLogIn);
 
-        //clean up
+        //attach event listener to inputs, so that on input change, errors are cleared
+        document.getElementById('input-email').addEventListener('input', clearError);
+        document.getElementById('input-password').addEventListener('input', clearError);
+
+        //clean up 
+        //input event listeners will be cleared when elements removed
         return () => {
             document.removeEventListener('submit', userLogIn);
         };
@@ -57,11 +70,13 @@ const LogInPage = () => {
                     <label htmlFor='password'>Password: </label>
                     <input type='password' id='input-password' name='password' required />
                 </div>
+                {errorData &&
+                    <span className='error-message'>{errorData.message}</span>
+                }
                 <div className='button-container login'>
                     <button className='button-link' type='submit'>Log In</button>
                     <Link className='button-link' to='/blog_reader'>Cancel</Link>
                 </div>
-
             </form>
             <p>Not yet signed up? Click <Link to='/blog_reader/sign-up'>here</Link>.</p>
         </div>
