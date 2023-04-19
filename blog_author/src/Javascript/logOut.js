53f73f9bd6017b71d127542f6ccd317a82723df6
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-19 16:28:22 +0100

    Create logOut.js

---
+++ blog_author/src/Javascript/logOut.js
@@ -0,0 +1,11 @@
+//request log out (clear jwt cookie) and redirect to blog list page
+const logOut = async () => {
+    try {
+        await fetch(`${process.env.REACT_APP_BLOGAPI_URL}/user/log-out`, { method: 'POST', credentials: 'include' });
+        window.location.href = '/blog_reader'; //-----------------------------------------------------------------------------------------
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default logOut;
