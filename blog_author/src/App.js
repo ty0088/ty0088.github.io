3269f25596895a27601a3cf1d156963cc466611d
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-19 10:14:18 +0100

    created skeleton app

---
+++ blog_author/src/App.js
@@ -0,0 +1,13 @@
+import './styles/style.css';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+
+    </Routes>
+  );
+}
+
+export default App;
