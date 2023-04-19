ba1d91858840f2effc66dea748b18458b16d1d75
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-19 16:28:27 +0100

    Create UserFormPage.js

---
+++ blog_author/src/Pages/UserFormPage.js
@@ -0,0 +1,44 @@
+import '../styles/formPages.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import UserForm from '../Components/UserForm';
+
+//check user is an author or admin ------------------------------------
+//alternative link for reader site -----------------------------------
+
+const UserFormPage = ({ action }) => {
+    const [errorData, setErrorData] = useState([]);
+    const [formFlag, setFormFlag] = useState(true);
+    const [userAuthFlag, setUserAuthFlag] = useState(false);
+
+    return (
+        <div id='main-container'>
+            <h1>The Blog Spot</h1>
+            {formFlag &&
+                <UserForm action={action} errorData={errorData} setErrorData={setErrorData} setFormFlag={setFormFlag} setUserAuthFlag={setUserAuthFlag} />
+            }
+            {(!formFlag && userAuthFlag) &&
+                <div>
+                    <p>You are trying to sign up as a new user but you are already logged in. </p>
+                    <p>Please try one of the links below: </p>
+                    <ul>
+                        <li><Link to='/blog_reader'>Home - Blog Posts</Link></li>
+                        <li><Link to='/blog_reader/log-out'>Log Out</Link></li>
+                    </ul>
+                </div>
+            }
+            {(!formFlag && !userAuthFlag) &&
+                <div>
+                    <p>You are trying to update your details but you are not logged in. </p>
+                    <p>Please try one of the links below: </p>
+                    <ul>
+                        <li><Link to='/blog_reader'>Home - Blog Posts</Link></li>
+                        <li><Link to='/blog_reader/log-in'>Log In</Link></li>
+                    </ul>
+                </div>
+            }
+        </div> 
+    );
+};
+export default UserFormPage;
