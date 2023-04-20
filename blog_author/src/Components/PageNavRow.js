c7b67ab904f11c89955abb4429a8f26b9e7f54aa
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-20 17:31:19 +0100

    Create PageNavRow.js

---
+++ blog_author/src/Components/PageNavRow.js
@@ -0,0 +1,18 @@
+import '../Styles/DashboardPage.css';
+import React from 'react';
+
+import PageNavSpan from './PageNavSpan';
+
+const PageNavRow = ({ paginateInfo, pageNum, sortOrd, limitVal }) => {
+    //Page navigation with sort by newest or oldest and query result limit
+    return (
+        <div className='post-info'>
+            <PageNavSpan paginateInfo={paginateInfo} sortOrd={sortOrd} limitVal={limitVal} />
+            &nbsp;-&nbsp;
+            Sort: <a href={`/blog_author?page=${pageNum}&sortOrd=-1&limit=${limitVal}`}>{(sortOrd === '-1' || sortOrd === '') ? <strong>Newest First</strong> : 'Newest First'}</a> : <a href={`/blog_author?page=${pageNum}&sortOrd=1&limit=${limitVal}`}>{sortOrd === '1' ? <strong>Oldest First</strong> : 'Oldest First'}</a>
+            &nbsp;-&nbsp;
+            Posts: <a href={`/blog_author?page=1&sortOrd=${sortOrd}&limit=5`}>{(limitVal === '5' || limitVal === '') ? <strong>5</strong> : '5'}</a> / <a href={`/blog_author?page=1&sortOrd=${sortOrd}&limit=10`}>{limitVal === '10' ? <strong>10</strong> : '10'}</a> / <a href={`/blog_author?page=1&sortOrd=${sortOrd}&limit=20`}>{limitVal === '20' ? <strong>20</strong> : '20'}</a>
+        </div>
+    );
+};
+export default PageNavRow;
