6d923256ff24a109de48859387757adecd9d1698
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-21 15:54:03 +0100

    on each location change, user is checked
    - check that user is logged in otherwise direct to log in page
    - check if user is a reader, if so redirect to reader site

--- blog_author/src/App.js
+++ blog_author/src/App.js
@@ -1,23 +1,61 @@
 import './Styles/style.css';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 
+import fetchUserToken from './Javascript/fetchUserToken';
+import redirectReader from './Javascript/redirectReader';
 import DashboardPage from './Pages/DashBoardPage';
 import LogInPage from './Pages/LogInPage';
 import UserFormPage from './Pages/UserFormPage'
 import NotFoundPage from './Pages/NotFoundPage';
 
 function App() {
-  
-  //Author Dashboard - page showing all author posts and CRUD mananagement of posts
-  //User Account Page - for user detail management
-  //Post Form Page - create/update posts
+  const [currUser, setCurrUser] = useState();
+  let location = useLocation();
+  const navigate = useNavigate();
+
+  //----------------------------------------------------------------------------------------
+  //Log In Page ----------- done
+  //Sign Up Page ----------- done
+  //Author Dashboard - page showing all author posts and CRUD mananagement of posts ---------- in progress
+  //User Account Page - for user detail management ------------ to do
+  //Post Form Page - create/update posts --------------- to do
+  //Post Detail Page - with comments ------------------- to do 
+  //----------------------------------------------------------------------------------------
+
+  useEffect(() => {
+    //on each location change check user type and set user state
+    const checkUser = async () => {
+      try {
+        //get user token
+        const userData = await fetchUserToken();
+        if (userData.user !== null) {
+          //if user token valid then check if user is a reader
+          redirectReader(userData.user);
+          //if author/admin, set user state
+          setCurrUser(userData.user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkUser();
+  }, [location]);
+
+  //check if user is logged in, if not redirect to log in page
+  useEffect(() => {
+    if (!currUser) {
+        navigate('/blog_author/log-in');
+    }
+  // eslint-disable-next-line
+  }, [currUser]);
 
   return (
     <Routes>
-        <Route path='/blog_author' element={<DashboardPage />} />
+        <Route path='/blog_author' element={<DashboardPage currUser={currUser} />} />
         <Route path='/blog_author/log-in' element={<LogInPage />} />
         <Route path='/blog_author/sign-up' element={<UserFormPage action={'create'} />} />
+        <Route path='/blog_author/user/:id'  />
         <Route path='/blog_author/user/:id/update' element={<UserFormPage action={'update'} />} />
         <Route path='*' element={<NotFoundPage />} />
     </Routes>
