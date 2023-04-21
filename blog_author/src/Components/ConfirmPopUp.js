2735bc5a02b2910253d9060507a1285c33820e33
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-21 16:03:12 +0100

    Create ConfirmPopUp.js

---
+++ blog_author/src/Components/ConfirmPopUp.js
@@ -0,0 +1,21 @@
+import '../Styles/ConfirmPopUp.css';
+import React from 'react';
+
+//This renders a pop up so that a user can confirm or cancel an action
+const ConfirmPopUp = ({name, cancelClick, confirmClick, message1, message2}) => {
+    return (
+        <div id='confirm-popup-container'>
+            <div className='confirm-popup'>
+                <span className='confirm-message1'>{message1}</span>
+                <span><strong>{name}</strong></span>
+                <span className='confirm-message2'>{message2}</span>
+                <div className='popup-btn-container'>
+                    <button type='button' className='button-link' onClick={confirmClick}>Confirm</button>
+                    <button type='button' className='button-link' onClick={cancelClick}>Cancel</button>
+                </div> 
+            </div>
+        </div>
+    );
+}
+
+export default ConfirmPopUp;
