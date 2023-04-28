5a859aa50b01c37912e29fbffa8a99ab4135b5a3
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-04-28 16:34:10 +0100

    only controllers to validate length

--- blog_api/models/comment.js
+++ blog_api/models/comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    text: { type: String, maxLength: 1000, required: true },
+    text: { type: String, required: true },
     post_date: { type: Date, default: Date.now },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
--- blog_api/models/user.js
+++ blog_api/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    display_name: { type: String, required: true, maxLength: 20 },
+    display_name: { type: String, required: true },
     password: { type: String, required: true},
     email: { type: String, required: true},
     join_date: { type: Date, default: Date.now },
