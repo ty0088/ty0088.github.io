3c2e23493e86519a06caa7e7c62d5c02d373dd82
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-05-16 11:53:13 +0100

    log out to redirect to home page
    To fix issue with log out on pages other than home page

--- blog_reader/src/javascript/logOut.js
+++ blog_reader/src/javascript/logOut.js
@@ -2,7 +2,7 @@
 const logOut = async () => {
     try {
         await fetch(process.env.NODE_ENV === 'production' ? `https://blog-api.ty0088.co.uk/user/log-out` : `${process.env.REACT_APP_BLOGAPI_URL}/user/log-out`, { method: 'POST', credentials: 'include' });
-        window.location.reload();
+        window.location.href = process.env.NODE_ENV === 'production' ? 'https://ty0088.github.io/blog_reader' : 'http://localhost:8000/blog_reader';
     } catch (error) {
         console.log(error);
     }
