2e8e6a8f1788aeb4836ad9dfdafbcc43c359c0db
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-06-06 14:35:56 +0100

    Selected buttons no longer selectable

--- blog_author/src/Components/PageNavRow.js
+++ blog_author/src/Components/PageNavRow.js
@@ -32,11 +32,17 @@ const PageNavRow = ({ paginateInfo, pageNum, sortOrd, limitVal, listViewFlag, se
             <div>
                 <PageNavSpan paginateInfo={paginateInfo} sortOrd={sortOrd} limitVal={limitVal} />&nbsp;-&nbsp;
             </div>
-            <div>
+            {/* <div>
                 Sort: <Link to={`/blog_author?page=${pageNum}&sortOrd=-1&limit=${limitVal}`}>{(sortOrd === '-1' || sortOrd === '') ? <strong>Newest First</strong> : 'Newest First'}</Link> / <Link to={`/blog_author?page=${pageNum}&sortOrd=1&limit=${limitVal}`}>{sortOrd === '1' ? <strong>Oldest First</strong> : 'Oldest First'}</Link>&nbsp;-&nbsp;
             </div>
             <div>
                 Posts: <Link to={`/blog_author?page=1&sortOrd=${sortOrd}&limit=5`}>{(limitVal === '5' || limitVal === '') ? <strong>5</strong> : '5'}</Link> / <Link to={`/blog_author?page=1&sortOrd=${sortOrd}&limit=10`}>{limitVal === '10' ? <strong>10</strong> : '10'}</Link> / <Link to={`/blog_author?page=1&sortOrd=${sortOrd}&limit=20`}>{limitVal === '20' ? <strong>20</strong> : '20'}</Link>
+            </div> */}
+            <div>
+                Sort: {(sortOrd === '-1' || sortOrd === '') ? <strong>Newest First</strong> : <Link to={`/blog_author?page=${pageNum}&sortOrd=-1&limit=${limitVal}`}>Newest First</Link>} / {sortOrd === '1' ? <strong>Oldest First</strong> : <Link to={`/blog_author?page=${pageNum}&sortOrd=1&limit=${limitVal}`}>Oldest First</Link>}&nbsp;-&nbsp;
+            </div>
+            <div>
+                Posts: {(limitVal === '5' || limitVal === '') ? <strong>5</strong> : <Link to={`/blog_author?page=1&sortOrd=${sortOrd}&limit=5`}>5</Link>} / {limitVal === '10' ? <strong>10</strong> : <Link to={`/blog_author?page=1&sortOrd=${sortOrd}&limit=10`}>10</Link>} / {limitVal === '20' ? <strong>20</strong> : <Link to={`/blog_author?page=1&sortOrd=${sortOrd}&limit=20`}>20</Link>}
             </div>
         </div>
     );
