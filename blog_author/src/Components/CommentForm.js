212cc0f5dd49833c4412693cae513419ca30e1eb
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-06-08 14:22:19 +0100

    corrected fetch URL for comment edit

--- blog_author/src/Components/CommentForm.js
+++ blog_author/src/Components/CommentForm.js
@@ -40,7 +40,7 @@ const CommentForm = ({ postId, commentText, fetchData, setNewComFlag, setScrollN
                 });
             } else {
                 //request update comment from api
-                    response = await fetch(process.env.NODE_ENV === 'production' ? `https://blog-api.ty0088.co.uk/post/${postId}/comment/update` : `${process.env.REACT_APP_BLOGAPI_URL}/post/${postId}/comment/update`, {
+                    response = await fetch(process.env.NODE_ENV === 'production' ? `https://blog-api.ty0088.co.uk/post/${postId}/comment/${editCommentId}/update` : `${process.env.REACT_APP_BLOGAPI_URL}/post/${postId}/comment/${editCommentId}/update`, {
                     method: 'PUT',
                     credentials: 'include',
                     headers: {
