349a138f51ab54370dfbc8125031c1fda66d6d96
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-06-19 15:32:03 +0100

    removed console.log

--- blog_author/src/Javascript/getS3ImageUrl.js
+++ blog_author/src/Javascript/getS3ImageUrl.js
@@ -3,7 +3,6 @@ const getS3ImageUrl = async (postId) => {
     try {
         const urlResponse = await fetch(process.env.NODE_ENV === 'development' ? `${process.env.REACT_APP_BLOGAPI_URL}/aws/getS3Url/${postId}` : `https://blog-api.ty0088.co.uk/aws/getS3Url/${postId}`, { credentials: 'include' });
         const { presignedGetUrl } = await urlResponse.json();
-        console.log(presignedGetUrl);
         //return image url, if image doesn't exists null will be returned by API
         return presignedGetUrl;
     } catch (error) {
