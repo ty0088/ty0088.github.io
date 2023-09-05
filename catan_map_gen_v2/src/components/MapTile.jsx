a97f9b21de035cfa80ebc39c825ac5a403722471
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-09-05 16:48:38 +0100

    Create MapTile.jsx

---
+++ catan_map_gen_v2/src/components/MapTile.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './styles/App.css';
+
+//props tileType, tokenValue
+const MapTile = ({tileType, tokenValue}) => {
+    
+    return (
+        <>
+        </>
+    );
+};
+
+export default MapTile;
