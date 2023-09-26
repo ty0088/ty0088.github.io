d9665dbf4247021c9a3961e44c84e54a561c6c0f
ty0088 <95297807+ty0088@users.noreply.github.com>
2023-09-26 18:02:09 +0100

    Added token value checking logic
    logic picking tile types and token values separated

--- catan_map_gen_v2/src/javascript/gameLogic.js
+++ catan_map_gen_v2/src/javascript/gameLogic.js
@@ -22,7 +22,7 @@ const dfMapTiles = { // default map tile data
     18: {q: -1, r: 2, tileType: null, tokenValue: null, sameType: 0},
     19: {q: 0, r: 2, tileType: null, tokenValue: null, sameType: 0},
 };
-const dfTileTypeQty = { // default number of tile types available
+const dfTileTypeQty = { // default number of tile types available - 1. Forest, 2. Pasture, 3. Field, 4. Hill, 5. Mountain, 6. Desert
     1: 4,
     2: 4,
     3: 4,
@@ -30,19 +30,32 @@ const dfTileTypeQty = { // default number of tile types available
     5: 3,
     6: 1,
 };
-const dfTokenValQty = { // default number of token values available
-        2: 1,
-        3: 2,
-        4: 2,
-        5: 2,
-        6: 2,
-        8: 2,
-        9: 2,
-        10: 2,
-        11: 2,
-        12: 1,
-};
 const totalTiles = Object.keys(dfTileTypeQty).reduce((acc, currKey) => acc += dfTileTypeQty[currKey], 0); // total number of tiles
+const dfTokenValQty = { // default number of token values available
+    2: 1,
+    3: 2,
+    4: 2,
+    5: 2,
+    6: 2,
+    8: 2,
+    9: 2,
+    10: 2,
+    11: 2,
+    12: 1,
+};
+const tokenProbVals = { // probability values for each tile value
+    0: 0,
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 4,
+    6: 5,
+    8: 5,
+    9: 4,
+    10: 3,
+    11: 2,
+    12: 1
+};
 const probLimit = 12; // probability limit for a group of 3 intersecting tiles
 const tileAdjLimit = 2; // limit of the same tiles types that are adjacent to each other
 const groupOffsets = [ // offset coords (q, r) to 2 other adjacent tiles for intersecting group relative to current tile
@@ -54,89 +67,153 @@ const groupOffsets = [ // offset coords (q, r) to 2 other adjacent tiles for int
     [[-1, 0], [0, -1]],
 ];
 
-// function generateMap()
-//  while (tileCount < 19)
-//      chose a random tile number that is still empty
-//      assign a random tile type that is still available, check it doesn't exceed adjacent tile limit
-//          if limit exceeded, pick a new tile type
-//          if no new tile type available, switch last picked tile type with current tile type and check both again
-//          if checks fail, keep going back and switching tiles until we run out of tiles to switch
-//          if no tile works, all tiles will need to be reset ??
-//      assign a random tile value that is still available (only 18 vals available, desert has no value), check it doesnt exceed adjacent probability limit
-//          if limit exceeded, pick a new value
-//          if no new token values available, switch last picked token val with current token val and check both again
-//          if checks fail, keep going back and switching tokens until we run out of tokens to switch
-//          if no token works, all tokens will need to be reset ??
-
 const generateMap = () => {
     //init default map properties
     let mapTiles = deepClone(dfMapTiles);
     console.log(deepClone(mapTiles));
-    let tileTypeQty = deepClone(dfTileTypeQty);
-    let tokenValQty = deepClone(dfTokenValQty);
+    let tileTypeQty = {...dfTileTypeQty};
+    let tokenValQty = {...dfTokenValQty};
     let tileCount = 0;
 
+    // get tile types
     while (tileCount < totalTiles) {
-        console.log(`- ${tileCount} -`);
+        console.log(`- ${tileCount + 1} -`);
         // get a random tile num that is still available
-        const currTileNum = pickTileNum(mapTiles);
+        const currTileNum = pickTileNum(mapTiles, 'tileType');
         console.log(`tileNum: ${currTileNum}`);
-        
+
         // chose a random tile type and check its valid
-        let currTileType = minMaxRandom(1, 6);
-        // keep track of any checked tile types
+        let {randElem, totalElems} = getRandom(tileTypeQty);
+        let currTileType = randElem;
+        console.log(`currentTileType: ${currTileType}`);
+        // keep track of any checked tile types and nums
         let checkedTypes = [currTileType];
         let checkedNums = [];
-        console.log(`currentTileType: ${currTileType}`);
-        // if tile type is not allowed, pick and check another random tile type
+
+        // if tile type is not allowed, reset or get another tile
         while (!checkTileType(currTileType, currTileNum, tileTypeQty, mapTiles)) {
-            console.log(`currentTileType: ${currTileType}`);
-            if (checkedTypes.length === 6) {   
-                // ---------------------------
-                // if no valid types left, reset map tiles
-                console.log('- RESET -');
-                mapTiles = deepClone(dfMapTiles);
+            console.log(checkedTypes);
+            console.log(`checked length: ${checkedTypes.length} & total length: ${totalElems}`);
+            if (checkedTypes.length === totalElems) {
+                // if no valid types left, reset tile types
+                console.log('- RESET TYPES -');
+                Object.keys(mapTiles).forEach(num => {
+                    mapTiles[num].tileType = null;
+                    mapTiles[num].sameType = 0;
+                });
                 console.log(deepClone(mapTiles));
-                tileTypeQty = deepClone(dfTileTypeQty);
+                tileTypeQty = {...dfTileTypeQty};
                 checkedNums = [];
                 checkedTypes = [];
                 tileCount = 0;
-                currTileType = minMaxRandom(1, 6);
+                ({randElem, totalElems} = getRandom(tileTypeQty));
+                currTileType = randElem;
+                console.log(`currentTileType: ${currTileType}`);
                 continue;
-            } else if (checkedTypes.includes(currTileType)) {
-                // if tile type has already been checked, get a new tile type and check again
-                currTileType = minMaxRandom(1, 6);
-            } else {
-                // if tile type has not been checked, add it to checked list
+            } else if (!checkedTypes.includes(currTileType)) {
+                // if tile type had not yet been checked, add it to checked list and get new tile type
                 checkedTypes.push(currTileType);
-                currTileType = minMaxRandom(1, 6);
+                ({randElem, totalElems} = getRandom(tileTypeQty));
+                currTileType = randElem;
+                console.log(`currentTileType: ${currTileType}`);
+            } else {
+                // if tile type has previously been checked, get a new random tile type
+                ({randElem, totalElems} = getRandom(tileTypeQty));
+                currTileType = randElem;
+                console.log(`currentTileType: ${currTileType}`);
             }
         }
 
+        // update tile type qty, sameType value and add current tile type to map
         console.log(`tile type to store: ${currTileType}`);
-
-        // update tile qty, sameType value and add current tile to map
         tileTypeQty[currTileType]--;
         updateSameType(currTileNum, currTileType, mapTiles, checkedNums);
-        mapTiles = {...mapTiles, [currTileNum]: {...mapTiles[currTileNum], tileType: currTileType}};
-        tileCount++;
+        mapTiles[currTileNum].tileType = currTileType;
 
+        tileCount++;
         console.log(deepClone(mapTiles));
     }
+
+    // reset tile count to get token values
+    tileCount = 0;
+
+    // get token values
+    while (tileCount < totalTiles) {
+        console.log(`- ${tileCount + 1} -`);
+        // get a random tile num that is still available
+        const currTileNum = pickTileNum(mapTiles, 'tokenValue');
+        console.log(`tileNum: ${currTileNum}`);
+
+        if (mapTiles[currTileNum].tileType === 6) {
+            // tile type is 6, assign token value 0
+            mapTiles[currTileNum].tokenValue = 0;
+        } else {
+            // tile type is NOT 6, assign random token value
+            let {randElem, totalElems} = getRandom(tokenValQty);
+            let currTokenVal = randElem;
+            console.log(`currTokenVal: ${currTokenVal}`);
+            // if token is not valid, reset or get new token
+            let checkedVals = [];
+    
+            while (!checkTokenValue(currTileNum, currTokenVal, tokenValQty, mapTiles)) {
+                console.log(checkedVals);
+                // no valid tokens left, reset
+                if (checkedVals.length === totalElems) {
+                    console.log('- RESET TOKENS -');
+                    Object.keys(mapTiles).forEach(num => mapTiles[num].tokenValue = null);
+                    console.log(deepClone(mapTiles));
+                    tokenValQty = {...dfTokenValQty};
+                    checkedVals = [];
+                    tileCount = 0;
+                    ({randElem, totalElems} = getRandom(tokenValQty));
+                    currTokenVal = randElem;
+                    console.log(`currTokenVal: ${currTokenVal}`);
+                    continue;
+                } else if (checkedVals.includes(currTokenVal)) {
+                    // if previously checked, get new token
+                    ({randElem, totalElems} = getRandom(tokenValQty));
+                    currTokenVal = randElem;
+                    console.log(`currTokenVal: ${currTokenVal}`);
+                } else {
+                    // if not previously checked, add to checked list and get new token
+                    checkedVals.push(currTokenVal);
+                    ({randElem, totalElems} = getRandom(tokenValQty));
+                    currTokenVal = randElem;
+                    console.log(`currTokenVal: ${currTokenVal}`);
+                }
+            }
+    
+            console.log(`token value to store: ${currTokenVal}`);
+            tokenValQty[currTokenVal]--;
+            mapTiles[currTileNum].tokenValue = currTokenVal;
+        }
+
+        tileCount++;
+        console.log(deepClone(mapTiles));
+    }
+
     return mapTiles;
 };
 
-// function to generate random integer between a min and max value
-const minMaxRandom = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+// function to return a random element and total elements available from qty array
+const getRandom = (obj) => {
+    const keys = Object.keys(obj).filter(key => obj[key] > 0);
+    if (keys.length === 0) return { randElem: false, totalElems: 0 };
+    console.log(`keys available: [${keys}]`); 
+    console.log(obj);
+    return {
+        randElem: parseInt(keys[Math.floor(Math.random() * keys.length)]),
+        totalElems: keys.length,
+    };
 };
 
 // function to pick a random available tile number
-const pickTileNum = (mapTiles) => {
+const pickTileNum = (mapTiles, keyName) => {
     while (true) {
         // pick a random tile number
-        const tileNum = minMaxRandom(1, totalTiles);
-        if (mapTiles[tileNum].tileType === null) {
+        const tileNum = Math.floor(Math.random() * totalTiles + 1);
+        console.log(tileNum);
+        if (mapTiles[tileNum][keyName] === null) {
             //if tile is not yet assigned a type, return tile num
             return tileNum;
         }
@@ -165,13 +242,15 @@ const checkTileType = (tileType, tileNum, tileTypeQty, mapTiles) => {
         if (adjTileNum0 && mapTiles[adjTileNum0].tileType == tileType) {
             groupCount++;
         }
-    }        
+    }   
+
     // check group does not exceed adj limit
     if (groupCount > tileAdjLimit) {
         console.log('group adj limit exceeded');
         return false;
     }
 
+    // if tile is availabe and does not exceed adj limits, return true
     return true;
 };
 
@@ -201,35 +280,37 @@ const updateSameType = (tileNum, tileType, mapTiles, checkedNums) => {
     } 
 };
 
-// function checkProbLimit(currQ, currR, tileVal, map) return true/false
-//  for each of the 6 intersecting tile groups
-//      if tile value doesnt exceed limit in group return true;
-//      else return false;
+// function to check if token value is valid
+const checkTokenValue = (currTileNum, currTokenVal, tokenValQty, mapTiles) => {
+    console.log('check token value is valid');
 
-// // start count at 1 for current tile type
-// let count = 1;
-// for (let offset of groupOffsets) {
-//     // find the number of the first and second adjacent tiles. If offset tile is same as current tile add to count
-//     // console.log(`mapTiles[tileNum].q: ${mapTiles[tileNum].q} + offset[0][0]: ${offset[0][0]} = ${mapTiles[tileNum].q + offset[0][0]}`);
-//     const adjTileNum0 = Object.keys(mapTiles).find(tile => mapTiles[tile].q === (mapTiles[tileNum].q + offset[0][0]) && mapTiles[tile].r === (mapTiles[tileNum].r + offset[0][1]));
-//     if (adjTileNum0 && mapTiles[adjTileNum0].tileType == tileType) {
-//         console.log(`tile 0 offset num: ${adjTileNum0}`);
-//         console.log(`Offset0 type: ${mapTiles[adjTileNum0].tileType} & tileType: ${tileType}`);
-//         count++;
-//     }
-//     const adjTileNum1 = Object.keys(mapTiles).find(tile => mapTiles[tile].q === mapTiles[tileNum].q + offset[1][0] && mapTiles[tile].r === mapTiles[tileNum].r + offset[1][1]);
-//     if (adjTileNum1 && mapTiles[adjTileNum1].tileType == tileType) {
-//         console.log(`tile 1 offset num: ${adjTileNum1}`);
-//         console.log(`Offset1 type: ${mapTiles[adjTileNum1].tileType} & tileType: ${tileType}`);
-//         count++;
-//     }
-//     console.log(`count: ${count}`);
-// }        
-// // if adj limit exceeded return false
-// if (count > tileAdjLimit) {
-//     return false;
-// }
+    // check token value is available
+    if (tokenValQty[currTokenVal] < 1) {
+        console.log(tokenValQty);
+        console.log('token not available');
+        return false;
+    }
 
-generateMap();
+    // check token value will not exceed prob limit
+    for (let offset of groupOffsets) {
+        // get the total probability value of each intersecting group
+        const adjTileNum0 = Object.keys(mapTiles).find(tile => mapTiles[tile].q === (mapTiles[currTileNum].q + offset[0][0]) && mapTiles[tile].r === (mapTiles[currTileNum].r + offset[0][1]));
+        const adjTileNum1 = Object.keys(mapTiles).find(tile => mapTiles[tile].q === (mapTiles[currTileNum].q + offset[1][0]) && mapTiles[tile].r === (mapTiles[currTileNum].r + offset[1][1]));
+        const currTileProb = tokenProbVals[currTokenVal];
+        const adjTileProb0 = mapTiles[adjTileNum0] ? mapTiles[adjTileNum0].tokenValue ? tokenProbVals[mapTiles[adjTileNum0].tokenValue] : 0 : 0;
+        const adjTileProb1 = mapTiles[adjTileNum1] ? mapTiles[adjTileNum1].tokenValue ? tokenProbVals[mapTiles[adjTileNum1].tokenValue] : 0 : 0;
+        const totalProb = currTileProb + adjTileProb0 + adjTileProb1;
+        console.log(`currTileProb: ${currTileProb} + adjTileProb0: ${adjTileProb0} + adjTileProb1: ${adjTileProb1}`);
+        console.log(`group total prob: ${totalProb}`);
+        if (totalProb > probLimit) {
+            console.log('probability limit exceeded');
+            return false;
+        }
+    } 
+
+    return true;
+};
+
+// generateMap();
 
 export default generateMap;
